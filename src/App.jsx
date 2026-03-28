3e7ab53b298097cfd9384300e2b0b2e6376b95ea
harshit <iamharshitsharma518@gmail.com>
2026-03-29 02:18:24 +0530

    harshitfirstcommit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+import Budget from './assets/components/Budget'
+function App() {
+  return (
+    <div>
+     <Budget />
+    </div>
+  )
+}
+
+export default App
